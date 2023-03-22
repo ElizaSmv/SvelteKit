8d1423d84c044a631db500794b0401377141664f
Eliza <lizavetta.vl@yandex.ru>
2023-03-23 02:01:06 +0300

    deploy: fix baaaaseeeee

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information photos adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : 'https://github.com/ElizaSmv/SvelteKit/'
+			base: dev ? '' : 'https://github.com/ElizaSmv/SvelteKit'
 		},
 	},
 };
