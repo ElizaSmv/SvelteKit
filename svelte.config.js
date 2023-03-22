b735def9f4c3942bb438b3f219462f13e8007518
Eliza <lizavetta.vl@yandex.ru>
2023-03-23 01:59:26 +0300

    deploy: fix baaaase

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information photos adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/SvelteKit'
+			base: dev ? '' : 'https://github.com/ElizaSmv/SvelteKit/'
 		},
 	},
 };
